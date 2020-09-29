30684b4a991c8a784941cef038c456136434ff3e
Md Rasel Khandkar <rasel15-7040@diu.edu.bd>
2020-09-30 01:13:31 +0600

    Path fixes

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,10 @@ import Home from '../views/Home.vue'
 import Create from '../views/Create.vue'
 import Update from '../views/Update.vue'
 
-import { folderName } from '../../build.config.json'
-const baseURL = process.env.NODE_ENV === 'production' ? `${folderName}/` : process.env.BASE_URL
+import { folderName, server } from '../../build.config.json'
+const baseURL = process.env.NODE_ENV === 'production'
+    ? `${server.folderPath}${folderName}/`
+    : process.env.BASE_URL
 
 Vue.use(VueRouter)
 
